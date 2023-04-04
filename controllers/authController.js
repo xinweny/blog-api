216c1689cb17c687d6eeefdde17a3b17b0b3c02c
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-04 17:35:13 -0400

    add register functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,54 @@
-const register = (req, res) => {
-  res.send('TODO: Implement sign up');
-};
+import passport from 'passport';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
+import { checkForValidationErrors, validateAndSanitizeUser } from '../utils/validators.js';
+
+import User from '../models/user.js';
+
+const register = [
+  ...validateAndSanitizeUser(User),
+  checkForValidationErrors,
+  async (req, res) => {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+      role: 'user',
+      createdAt: new Date(),
+    });
+
+    await user.save();
+
+    res.status(200).json({
+      status: 200,
+      message: 'User created successfully.',
+    });
+  }
+];
 
 const login = (req, res) => {
-  res.send('TODO: Implement login');
+  passport.authenticate('local', { session: 'false' },
+    (err, user, info) => {
+      if (err || !user) return res.status(400).json({
+        status: 400,
+        message: info.message,
+      });
+
+      req.login(user, { session: 'false' }, err => {
+        if (err) return res.status(400).json({
+          status: 400,
+          message: err,
+        });
+      });
+
+      const token = jwt.sign(user, process.env.JWT_SECRET_KEY);
+
+      return res.json({ user, token });
+    }
+  )(req, res);
 };
 
 const logout = (req, res) => {
