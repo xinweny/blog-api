ee5bb23058e71722d63edb1636ce7849ac5723aa
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-04 16:41:13 -0400

    move authrouter into apirouter

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ import configMongoDB from './config/mongoConfig.js';
 import configPassport from './config/passportConfig.js';
 
 import apiRouter from './routes/api.js';
-import authRouter from './routes/auth.js';
 
 configMongoDB();
 
@@ -13,7 +12,6 @@ const app = express();
 
 configPassport();
 
-app.use('/', authRouter);
 app.use('/api', apiRouter);
 
 app.listen(3000, () => console.log('Server started on port 3000'));
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
+import authRouter from './auth.js';
 import userRouter from './users.js';
 import postRouter from './posts.js';
 import commentRouter from './comments.js';
 import likeRouter from './likes.js';
 
 const apiRouter = Router();
+
+apiRouter.use('/', authRouter);
 
 apiRouter.use('/users', userRouter);
 
