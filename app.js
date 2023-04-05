2661a72074c64c0d770b8a8f5b1222bf52358317
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-05 13:01:12 -0400

    fix bug in producing error response object

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from 'express';
 
 import configMongoDB from './config/mongoConfig.js';
 import configPassport from './config/passportConfig.js';
-import { handle404Error, handleServerError } from './config/errorsConfig.js';
+import handleErrors from './config/errorsConfig.js';
 
 import apiRouter from './routes/api.js';
 
@@ -20,7 +20,6 @@ configPassport(app);
 app.use('/api', apiRouter);
 
 // ERROR HANDLING
-handle404Error(app);
-handleServerError(app);
+handleErrors(app);
 
 app.listen(3000, () => console.log('Server started on port 3000'));
--- config/errorsConfig.js
+++ config/errorsConfig.js
@@ -1,22 +1,17 @@
-const handle404Error = app => {
+const handleErrors = app => {
   app.use((req, res, next) => {
     const err = new Error('Resource not found.');
     err.status = 404;
     next(err);
   });
-}
 
-const handleServerError = app => {
-  app.use((err, req, res) => {
+  app.use((err, req, res, next) => {
     res.status(err.status || 500);
 
     res.json({
       error: { code: err.status, message: err.message },
     });
   });
-};
-
-export {
-  handle404Error,
-  handleServerError,
 }
+
+export default handleErrors;
