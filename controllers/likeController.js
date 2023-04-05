01045f08acc65db312c64a03edd65be3c8cb24ae
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-05 15:01:33 -0400

    add POST route

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -1,5 +1,30 @@
-const likePost = (req, res) => {
-  res.send('TODO: Implement like post');
+import Like from '../models/like.js';
+
+import { customError } from '../utils/error.js';
+
+const likePost = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const { postId } = req.params;
+
+    const likeExists = await Like.exists({ userId, postId });
+
+    if (likeExists) throw customError(400, 'User has already liked this post.');
+
+    const like = new Like({
+      userId,
+      postId,
+    });
+
+    await like.save();
+
+    res.json({
+      data: { like },
+      message: 'Post liked by user successfully.',
+    });
+  } catch (err) {
+    return next(err);
+  }
 };
 
 const unlikePost = (req, res) => {
