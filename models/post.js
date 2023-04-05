d23224ac48e55bc250ae5ccd6724c3fa580b898c
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-05 12:42:12 -0400

    add likes count for easy reference

--- models/post.js
+++ models/post.js
@@ -4,6 +4,7 @@ const PostSchema = new Schema({
   userId: { type: Schema.ObjectId, ref: 'User', required: true },
   title: { type: String, required: true, minLength: 1 },
   text: { type: String, required: true, minLength: 1 },
+  likesCount: { type: Number, required: true, default: 0 },
   createdAt: { type: Date, required: true },
   publishedAt: { type: Date },
 });
