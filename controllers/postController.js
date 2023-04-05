99794288b7aea254a91683100a85d744f0874d20
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-05 13:02:40 -0400

    add GET routes for posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,9 +1,27 @@
-const listPosts = (req, res) => {
-  res.send('TODO: List all posts');
+import Post from '../models/post.js';
+import Comment from '../models/comment.js';
+
+const listPosts = async (req, res, next) => {
+  try {
+    const posts = await Post.find({}).populate('user');
+
+    res.json({ data: { posts } });
+  } catch (err) {
+    return next(err);
+  }
 };
 
-const getPost = (req, res) => {
-  res.send('TODO: Get post by ID');
+const getPost = async (req, res, next) => {
+  try {
+    const [post, comments] = await Promise.all([
+      Post.findById(req.params.postId).populate('user'),
+      Comment.find({ postId: req.params.postId }).populate('user'),
+    ]);
+
+    res.json({ data: { post, comments } });
+  } catch (err) {
+    return next(err);
+  }
 };
 
 const createPost = (req, res) => {
