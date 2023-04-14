2634b950a988426658c592c0264a3b0d8e7edbf3
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-14 15:09:14 -0400

    fix bug in includeKeys

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,7 @@
 const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
 
 const includeKeys = (obj, incKeys) => {
-  return obj
+  return Object.keys(obj)
     .filter(k => incKeys.includes(k))
     .reduce((o, k) => {
       o[k] = obj[k];
