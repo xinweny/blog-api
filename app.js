bd31b0147f74b7688ab5623e11e4f48d8614c538
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-24 19:17:42 -0400

    set extended to false

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 // Parsers
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(cors());
 app.use(compression());
