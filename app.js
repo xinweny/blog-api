4f75ca2146ee8dc489bd594b079660cc32d6babc
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-24 17:21:46 -0400

    set extended to true

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ configMongoDB();
 
 const app = express();
 
+// Parsers
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
 app.use(compression());
 app.use(helmet());
